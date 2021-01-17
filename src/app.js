a1239f13c17eaa67e5160be6b88f109d6b3510b7
Manoj Rana <rana.manoj008@gmail.com>
2021-01-17 21:27:04 +0530

    reset

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ const Registration = require('./models/Registration');
 const bcrypt = require('bcryptjs');
 const cookieParser = require('cookie-parser');
 const auth = require('./midelware/auth');
+const nodemailer = require('nodemailer');
 
 const viewsPath = path.join(__dirname, '../templates/views');
 const partialsPath = path.join(__dirname, '../templates/partials');
@@ -21,7 +22,6 @@ app.use(express.urlencoded({extended:false}))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, '../public')))
 
-console.log(process.env.SECRET_KEY)
 
 app.get('/' , (req,res)=>{
     res.render('index.hbs')
@@ -98,6 +98,182 @@ app.post('/login', async(req,res)=>{
     }
 })
 
+// fogot Password
+
+app.get('/forgot', (req,res)=>{
+    res.render('Forgot')
+}) 
+
+const genRanpass = (length)=>{
+    let result           = '';
+    let characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let charactersLength = characters.length;
+    for ( let i = 0; i < length; i++ ) {
+       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
+
+app.post('/forgot', async(req,res)=>{
+    try{
+        // let email = req.body.email;
+        // let user = req.body.username;
+    
+        let email = req.body.email;
+        let user = req.body.username;
+    
+        // console.log(username);
+        // console.log(email);
+    
+        let validUser = await Registration.findOne( {$and: [ { email : email}, {username : user}]})
+        // console.log(validUser)
+    
+        // ********************************************************
+    
+        if(validUser){
+            
+        const transporter = nodemailer.createTransport({
+            host : 'smtp.gmail.com',
+            port : 587,
+            secure : false,
+            auth : {
+                user : 'rana.manoj008@gmail.com',
+                pass : 'secrethai@123'
+            }
+        })
+    
+        // const mailOption = {
+        //     from : 'rana.manoj008@gmail.com',
+        //     to : 'rana.manoj008@gmail.com',
+        //     subject : "demo hai bs",
+        //     text : genRanpass(8)
+        // }
+        
+        const mailOption = {
+            from : 'rana.manoj008@gmail.com',
+            to : 'rana.manoj008@gmail.com',
+            subject : "demo hai bsss....",
+            text : '**************',
+            html: '<p>Click <a href="http://localhost:8000/reset/:email ' + email + '">here</a> to reset your password</p>'
+    
+        }
+    
+        transporter.sendMail(mailOption,(err,data)=>{
+            if(err){
+                console.log(err)
+            }else{
+                // console.log(data.response)
+            res.send('password reset mail has been sent to your email address.')
+            
+            }
+        })
+        }else{
+            console.log(`user does not exist`)
+        }
+    
+        // **************************************************************
+    
+        // if(validUser){
+        //     res.send('password reset mail has been sent to your email address.')
+        // }
+    
+    }catch(e){
+        console.log(e)
+    }
+})
+
+app.get('/reset/:email', async(req,res)=>{
+    res.render('reset')
+})
+
+app.post('/reset/:email', async(req,res)=>{
+    try{
+        
+        const pass = req.body.password;
+        const Cpass = req.body.Cpassword;
+        
+
+        // if(pass === Cpass){
+        //     const email = req.params.email;
+        //      await Registration.findOneAndUpdate(email, req.body, {new:false})   
+        //     res.render('login')
+        //     console.log('updated')
+
+        // }
+
+    }catch(e){
+        console.log(e)
+    }
+})
+
+// app.post('/forgot', async(req,res)=>{
+//     try{
+//         // let email = req.body.email;
+//         // let user = req.body.username;
+    
+//         let email = req.body;
+//         let user = req.body;
+    
+//         // console.log(username);
+//         // console.log(email);
+    
+//         let validUser = await Registration.findOne( {$and: [ { email : email}, {username : user}]})
+//         // console.log(validUser)
+    
+//         // ********************************************************
+    
+//         if(validUser){
+            
+//         const transporter = nodemailer.createTransport({
+//             host : 'smtp.gmail.com',
+//             port : 587,
+//             secure : false,
+//             auth : {
+//                 user : 'rana.manoj008@gmail.com',
+//                 pass : 'secrethai@123'
+//             }
+//         })
+    
+//         // const mailOption = {
+//         //     from : 'rana.manoj008@gmail.com',
+//         //     to : 'rana.manoj008@gmail.com',
+//         //     subject : "demo hai bs",
+//         //     text : genRanpass(8)
+//         // }
+        
+//         const mailOption = {
+//             from : 'rana.manoj008@gmail.com',
+//             to : 'rana.manoj008@gmail.com',
+//             subject : "demo hai bsss....",
+//             text : '**************',
+//             html: '<p>Click <a href="http://localhost:8000/register">here</a> to reset your password</p>'
+    
+//         }
+    
+//         transporter.sendMail(mailOption,(err,data)=>{
+//             if(err){
+//                 console.log(err)
+//             }else{
+//                 console.log(data.response);
+//                 Registration.updateOne({email : email})
+//             }
+//         })
+//         }else{
+//             console.log(`user does not exist`)
+//         }
+    
+//         // **************************************************************
+    
+//         // if(validUser){
+//         //     res.render('about')
+//         // }
+    
+//     }catch(e){
+//         console.log(e)
+//     }
+// })
+
 
 // form ends
 
@@ -110,4 +286,4 @@ app.get('*' , (req,res)=>{
 
 app.listen(port,()=>{
     console.log(`port ${port}`)
-})
+})cd
--- src/midelware/auth.js
+++ src/midelware/auth.js
@@ -18,4 +18,16 @@ const auth = async(req,res,next)=>{
 
 }
 
+// const genRanpass = (length)=>{
+//     let result           = '';
+//     let characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+//     let charactersLength = characters.length;
+//     for ( let i = 0; i < length; i++ ) {
+//        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+//     }
+//     return result;
+// }
+
+// genRanpass(5);
+
 module.exports = auth;
